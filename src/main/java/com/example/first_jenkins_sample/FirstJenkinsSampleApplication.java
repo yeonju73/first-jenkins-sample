01da7ec7c8e3b5f6b76844273a08e38b3db9b63e
yeonju73 <yeon703@naver.com>
2026-04-09 14:12:42 +0900

    init: first commit

---
+++ src/main/java/com/example/first_jenkins_sample/FirstJenkinsSampleApplication.java
@@ -0,0 +1,13 @@
+package com.example.first_jenkins_sample;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FirstJenkinsSampleApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FirstJenkinsSampleApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/first_jenkins_sample/FirstJenkinsSampleApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.first_jenkins_sample;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FirstJenkinsSampleApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
