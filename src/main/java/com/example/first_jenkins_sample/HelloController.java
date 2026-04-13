942152b747653b19468b65242e2d7ef28d5829e7
yeonju73 <yeon703@naver.com>
2026-04-13 14:39:43 +0900

    fix: add ?

--- src/main/java/com/example/first_jenkins_sample/HelloController.java
+++ src/main/java/com/example/first_jenkins_sample/HelloController.java
@@ -8,6 +8,6 @@ public class HelloController {
 
     @GetMapping("/hello")
     public String hello() {
-        return "Hello, Jenkins CI/CD!!!!!";
+        return "Hello, Jenkins CI/CD?";
     }
 }
